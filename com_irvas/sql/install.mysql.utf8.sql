1af53adb3ab5b8d318919624776e904a6c2b07fa
aleksandar-irvas <aca_fpi@hotmail.com>
2015-08-05 22:17:20 +0200

    dodat sql upit

---
+++ com_irvas/sql/install.mysql.utf8.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS `#__irvas_contacts` (
+`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+`title` varchar(250) NOT NULL DEFAULT '',
+`name` varchar(255) NOT NULL DEFAULT '',
+PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
