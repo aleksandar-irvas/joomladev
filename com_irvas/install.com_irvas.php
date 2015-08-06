149e0322528680c3f0270de11654fa5d1a2def69
aleksandar-irvas <aca_fpi@hotmail.com>
2015-08-06 13:17:33 +0200

    napokon radi

---
+++ com_irvas/install.com_irvas.php
@@ -0,0 +1,22 @@
+<?php 
+defined('_JEXEC') or die;
+
+/*function com_install(){
+   global $database;
+
+   //get database
+
+    $database = JFactory::getDBO();
+ 
+   
+   //table for item index
+   $database->setQuery("CREATE TABLE IF NOT EXISTS `#__irvas_contacts` (
+   `id` int(10) unsigned NOT NULL AUTO_INCREMENT, 
+   `title` varchar(250) NOT NULL DEFAULT '', 
+   `name` varchar(255) NOT NULL DEFAULT '', 
+   PRIMARY KEY (`id`)
+   ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;");
+    $database->query();
+
+}*/
+?>
